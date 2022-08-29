0c442cc8d9b89ec8e96989b54f31d43a9ef3582a
castrojo012495 <100735180+castrojo012495@users.noreply.github.com>
2022-08-29 14:53:16 -0700

    Initial commit

---
+++ CIS_17A_Hello_World/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: darthcastle
+ *
+ * Created on August 29, 2022, 2:36 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello, World!" ;
+    return 0;
+}
+
